b3ad0c3be619fcbbbd84213a66c4c87977e78ed7
Domenick Mitchell <domenickmitchell00@gmail.com>
2021-08-13 10:19:49 -0400

    Template for Program

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Drawing;
+using Pastel;
 
 namespace RhythmsGonnaGetYou
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+        static void Main(string[] args)
+        {
+            // Hippo from https://www.asciiart.eu/animals/other-land
+            Console.Clear();
+            Console.WriteLine($"{"#     #              ".Pastel(Color.SeaGreen)}{"      ".Pastel(Color.SteelBlue)}{" ###       ".Pastel(Color.LightSkyBlue)}{" ######                            ".Pastel(Color.MediumVioletRed)} {@"      c~~p ,---------.    ".Pastel(Color.SteelBlue)}");
+            Console.WriteLine($"{"#     #  ####  ##### ".Pastel(Color.SeaGreen)}{"      ".Pastel(Color.SteelBlue)}{"  #  ##### ".Pastel(Color.LightSkyBlue)}{" #     # ######   ##   #####  #### ".Pastel(Color.MediumVioletRed)} {@" ,---'".Pastel(Color.SteelBlue)}{"oo".Pastel(Color.ForestGreen)}{@"  )           \".Pastel(Color.SteelBlue)}");
+            Console.WriteLine($"{"#     # #    # #    #".Pastel(Color.SeaGreen)}{"      ".Pastel(Color.SteelBlue)}{"  #  #    #".Pastel(Color.LightSkyBlue)}{" #     # #       #  #    #   #     ".Pastel(Color.MediumVioletRed)} {@"(".Pastel(Color.SteelBlue)}{" O O".Pastel(Color.DimGray)}{"                  )/ ".Pastel(Color.SteelBlue)}");
+            Console.WriteLine($"{"####### #    # #    #".Pastel(Color.SeaGreen)}{" #####".Pastel(Color.Violet)}{"  #  #    #".Pastel(Color.LightSkyBlue)}{" ######  #####  #    #   #    #### ".Pastel(Color.MediumVioletRed)} {@" `".Pastel(Color.SteelBlue)}{"=".Pastel(Color.WhiteSmoke)}{"^".Pastel(Color.LightPink)}{"=".Pastel(Color.WhiteSmoke)}{@"'                 /".Pastel(Color.SteelBlue)}");
+            Console.WriteLine($"{"#     # #    # ##### ".Pastel(Color.SeaGreen)}{"      ".Pastel(Color.SteelBlue)}{"  #  ##### ".Pastel(Color.LightSkyBlue)}{" #     # #      ######   #        #".Pastel(Color.MediumVioletRed)} {@"       \    ,     .   /   ".Pastel(Color.SteelBlue)}");
+            Console.WriteLine($"{"#     # #    # #     ".Pastel(Color.SeaGreen)}{"      ".Pastel(Color.SteelBlue)}{"  #  #     ".Pastel(Color.LightSkyBlue)}{" #     # #      #    #   #   #    #".Pastel(Color.MediumVioletRed)} {@"       \".Pastel(Color.DimGray)}{@"\".Pastel(Color.SteelBlue)}{@"  |-----'|  /    ".Pastel(Color.SteelBlue)}");
+            Console.WriteLine($"{"#     #  ####  #     ".Pastel(Color.SeaGreen)}{"      ".Pastel(Color.SteelBlue)}{" ### #     ".Pastel(Color.LightSkyBlue)}{" ######  ###### #    #   #    #### ".Pastel(Color.MediumVioletRed)} {@"       |".Pastel(Color.DimGray)}{"|__|".Pastel(Color.SteelBlue)}    {"|_".Pastel(Color.DimGray)}{"|__|".Pastel(Color.SteelBlue)}\n");
+            Console.WriteLine($"{"############################################################################################################".Pastel(Color.DarkGoldenrod)}");
+            Console.WriteLine($"{"############################################################################################################".Pastel(Color.DarkGoldenrod)}");
+        }
     }
-  }
 }
--- queries.sql
+++ queries.sql
@@ -1,6 +1,7 @@
 RecordsDatabase
 
 -- Day 1 of Rhythm's GGY (ERD)
+-- Explorer Mode Step (1-16) Done!
 
 -- Step 1
 createdb RecordsDatabase
@@ -9,12 +10,46 @@ createdb RecordsDatabase
 pgcli RecordsDatabase
 
 -- Step 3
-CREATE TABLE "Albums" ("Id" SERIAL PRIMARY KEY, "Title" TEXT, "Genre" TEXT, "IsExplicit" BOOLEAN, "ReleaseDate" DATE);
+CREATE TABLE "Albums" ("Id" SERIAL PRIMARY KEY, "Title" TEXT, "IsExplicit" BOOLEAN, "ReleaseDate" DATE);
 
 -- Step 4
-CREATE TABLE "Bands" ("Id" SERIAL PRIMARY KEY, "Name" TEXT, "CountryOfOrigin" TEXT, "Website" TEXT, "Style" TEXT, "IsSigned" BOOLEAN, "ContactName" TEXT, "ContactPhoneNumber" VARCHAR(10));
+CREATE TABLE "Bands" ("Id" SERIAL PRIMARY KEY, "Name" TEXT, "CountryOfOrigin" TEXT, "NumberOfMembers" INTEGER, "Website" TEXT, "Style" TEXT, "IsSigned" BOOLEAN, "ContactName" TEXT, "ContactPhoneNumber" VARCHAR(12));
 
 -- Step 5
 CREATE TABLE "Songs" ("Id" SERIAL PRIMARY KEY, "TrackNumber" INT, "Title" TEXT, "Duration" TIME,);
 
--- Step 6
+-- Step 6 (Relationship between Entities)
+ALTER TABLE "Bands" ADD COLUMN "AlbumId" INTEGER NULL REFERENCES "Albums" ("Id");
+
+-- Step 7
+INSERT INTO "Bands" ("Id", "Name", "CountryOfOrigin", "NumberOfMembers", "Website", "Style", "IsSigned", "ContactName", "ContactPhoneNumber"); 
+VALUE (1, 'Juice WRLD', 'USA', 1, 'https://999club.com/', 'Hip-Hop/Rap', 'True', 'Jarad Higgins', '123-456-7890');
+
+-- Step 8
+SELECT * From "Bands";
+
+-- Step 9
+INSERT INTO "Albums" ("Id", "Title", "IsExplicit", "ReleaseDate")
+VALUES (1, 'Legends Never Die', 'True', '07-10-2020');
+
+-- Step 10
+INSERT INTO "Songs" ("Id", "TrackNumber", "Title", "Duration")
+VALUES (1, 3, 'Titanic', '2:57');
+
+-- Step 11
+UPDATE "Bands" SET "IsSigned" = 'False' WHERE "Name" = 'Juice WRLD';
+
+-- Step 12
+UPDATE "Bands" SET "IsSigned" = 'True' WHERE "Name" = 'Juice WRLD';
+
+-- Step 13
+SELECT "Albums"."Title" FROM "Albums" JOIN "Bands" ON "Albums"."Id" = "Bands"."Id" WHERE "Bands"."Name" = 'Juice WRLD';
+
+-- Step 14
+SELECT * FROM "Albums" ORDER BY "ReleaseDate"
+
+-- Step 15
+SELECT "Name" FROM "Bands" WHERE "IsSigned" = 'True'
+
+-- Step 16
+SELECT "Name" FROM "Bands" WHERE "IsSigned" = 'False'
